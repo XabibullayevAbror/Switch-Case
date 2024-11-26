f0389363ead821afd2164d477d6fda296b084930
XabibullayevAbror <abrorboiiixabibullayev@gmail.com>
2024-11-26 22:15:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let baho = prompt('TANLANG A, B, C')
+
+
+switch (baho.toUpperCase()){
+    case 'A':
+        console.log('5GA OQIYSIZ');
+        break;
+     case 'B':
+     console.log('4GA OQIYSIZ');
+     break
+     case 'C':
+     console.log('3GA OQIYSIZ');
+     break
+     default:
+        console.log('YOQOLGIN BUYODAN');
+}
